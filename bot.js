1a919e2afeb8e8c05a95d4b7f7983681a6017021
jlsajfj <jlsajfj@hotmail.com>
2020-12-16 17:50:05 -0500

    swapped service, to directly ping server instead of going through 3rd party apis. Added timestamp for when test is executed

--- bot.js
+++ bot.js
@@ -1,67 +1,91 @@
 const Discord = require('discord.js');
+const Query = require("minecraft-query");
+
 const bot = new Discord.Client();
 const config = require('./config.json');
-bot.login(config.token); //Add your own Discord bot token
+bot.login(config.token);
 
-const prefix = config.prefix //Bot command prefix
-var request = require('request');
-var CMD = 'ping'; //Command to trigger
-var mcIP = config.ip; //Add your Minecraft server IP
-var mcPort = config.port; //The port of the server, default it 25565
-var serverName = 'KOTH'; //Your server name
-var serverUrl = ""; //Server website
-var serverLogo = "https://i.imgur.com/szHlGja.png"; //Server logo
+var mcIP = config.ip;
+var dIP = config.displayip;
+var mcPort = config.port;
+var serverName = 'KOTH';
+var serverLogo = "https://i.imgur.com/szHlGja.png";
 var ch;
 
-function check(){
-    var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mcPort;
-	ch.bulkDelete(100).then(messages => console.log(`Bulk deleted ${messages.size} messages`)).catch(console.error);
-    request(url, function (err, response, body) {
-      if (err) {
-        console.log(err);
-        return message.reply('Error getting Minecraft server status...');
-      }
-      
-      body = JSON.parse(body);
-      var status = "Offline"
-      var color = 16711680
-      if (body.online) {
-        status = "Online";
-        color = 65280
-      }
-      var players = 0
-      if (body.players.now) {
-        players += body.players.now;
-      }
-      else {
-        players += 0
-      }
-      
-      const embed = {
-        "author": {
-          "name": serverName + " Server Status",
-          "url": serverUrl,
-          "icon_url": serverLogo
-        },
-        "color": color,
-        "fields": [
-          {
-            "name": "Status:",
-            "value": status,
-            "inline": true
-          },
-          {
-            "name": "Players Online:",
-            "value": "**" + body.players.now + "** / **" + body.players.max + "**",
-            "inline": true
-          }
-        ],
-        "footer": {
-          "text": "IP: " + mcIP
-        }
-      };
-      ch.send({ embed });
-    });
+var q;
+var embed;
+var status;
+var color;
+var players;
+var time;
+var body;
+async function check(){
+	q = new Query({host: mcIP, port: mcPort, timeout: 7500});
+	// ch.bulkDelete(100).then(messages => console.log(`Bulk deleted ${messages.size} messages`)).catch(console.error);
+	await q.basicStat().then(s => {
+		console.log("online");
+		//console.log(s);
+		q.close();
+		status = "Online";
+		color = 65280;
+		players = "**" + s.online_players + "** / **" + s.max_players + "**";
+		time = new Date().toLocaleString();
+		embed = {
+			"author": {
+				"name": serverName + " Server Status",
+				"icon_url": serverLogo
+			},
+			"color": color,
+			"fields": [
+				{
+					"name": "Status:",
+					"value": status,
+					"inline": true
+				},
+				{
+					"name": "Players Online:",
+					"value": players,
+					"inline": true
+				}
+			],
+			"footer": {
+				"text": "IP: " + dIP + "\nUpdated at: " + time
+			}
+		};
+		msg.edit({ embed });
+	}).catch(e => {
+		console.log("offline");
+		//console.log(e);
+		status = "Offline"
+		color = 16711680
+		players = "**0** / **0**";
+		time = new Date().toLocaleString();
+		embed = {
+			"author": {
+				"name": serverName + " Server Status",
+				"icon_url": serverLogo
+			},
+			"color": color,
+			"fields": [
+				{
+					"name": "Status:",
+					"value": status,
+					"inline": true
+				},
+				{
+					"name": "Players Online:",
+					"value": players,
+					"inline": true
+				}
+			],
+			"footer": {
+				"text": "IP: " + dIP + "\nUpdated at: " + time
+			}
+		};
+		msg.edit({ embed });
+	});
+	console.log(time);
+	console.log();
 }
 
 
@@ -70,8 +94,11 @@ bot.on('ready', () => {
 	bot.channels.fetch(config.channel).then(channel => {
 		console.log(channel.name);
 		ch=channel;
-		check();
+		ch.messages.fetch(config.message).then(message => {
+			console.log(message.author);
+			msg = message;
+			check();
+		}).catch(console.error);
 	}).catch(console.error);
-	bot.user.setActivity(prefix + CMD);
 	setInterval(check,30000);
 });
