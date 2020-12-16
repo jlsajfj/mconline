bfb5153f2b5f0184d1933b7cd9844cb7ff50afa0
jlsajfj <jlsajfj@hotmail.com>
2020-12-16 13:45:57 -0500

    just getting started

---
+++ bot.js
@@ -0,0 +1,73 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const config = require('./config.js');
+bot.login(config.token); //Add your own Discord bot token
+
+const prefix = config.prefix //Bot command prefix
+var request = require('request');
+var CMD = 'ping'; //Command to trigger
+var mcIP = config.ip; //Add your Minecraft server IP
+var mcPort = config.port; //The port of the server, default it 25565
+var serverName = 'KOTH'; //Your server name
+var serverUrl = ""; //Server website
+var serverLogo = "https://images-eu.ssl-images-amazon.com/images/I/512dVKB22QL.png"; //Server logo
+
+bot.on('message', message => {
+
+  if (message.content === prefix + CMD) {
+    var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mcPort;
+	var ch = message.channel;
+	ch.bulkDelete(100).then(messages => console.log(`Bulk deleted ${messages.size} messages`)).catch(console.error);
+    request(url, function (err, response, body) {
+      if (err) {
+        console.log(err);
+        return message.reply('Error getting Minecraft server status...');
+      }
+      
+      body = JSON.parse(body);
+      var status = "Offline"
+      var color = 16711680
+      if (body.online) {
+        status = "Online";
+        color = 65280
+      }
+      var players = 0
+      if (body.players.now) {
+        players += body.players.now;
+      }
+      else {
+        players += 0
+      }
+      
+      const embed = {
+        "author": {
+          "name": serverName + " Server Status",
+          "url": serverUrl,
+          "icon_url": serverLogo
+        },
+        "color": color,
+        "fields": [
+          {
+            "name": "Status:",
+            "value": status,
+            "inline": true
+          },
+          {
+            "name": "Players Online:",
+            "value": "**" + body.players.now + "** / **" + body.players.max + "**",
+            "inline": true
+          }
+        ],
+        "footer": {
+          "text": "IP: " + mcIP
+        }
+      };
+      ch.send({ embed });
+    });
+  };
+});
+
+bot.on('ready', () => {
+  console.log("Bot is active");
+  bot.user.setActivity(prefix + CMD)
+});
